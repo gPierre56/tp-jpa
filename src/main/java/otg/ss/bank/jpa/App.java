0891c574df168b34478a0044b142a929db23a6fc
Guillaume Pierre <guillaumepierre56@gmail.com>
2019-08-27 15:38:24 +0200

    fin tp jpa

--- src/main/java/otg/ss/bank/jpa/App.java
+++ src/main/java/otg/ss/bank/jpa/App.java
@@ -1,8 +1,74 @@
 package otg.ss.bank.jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import otg.ss.bank.jpa.domain.Account;
+import otg.ss.bank.jpa.domain.Address;
+import otg.ss.bank.jpa.domain.Agency;
+import otg.ss.bank.jpa.domain.ChargedAccount;
+import otg.ss.bank.jpa.domain.SimpleAccount;
+import otg.ss.bank.jpa.domain.Transaction;
+import otg.ss.bank.jpa.domain.Transaction.Type;
+import otg.ss.bank.jpa.domain.TransactionId;
+
 public class App {
-	
-	public static void main( String... args ) {
-	
+
+	public static void main(String... args) {
+
+		EntityManagerFactory emf = null;
+		EntityManager em = null;
+
+		try {
+			emf = Persistence.createEntityManagerFactory("persist-unit");
+			em = emf.createEntityManager();
+			em.getTransaction().begin();
+			Agency agency = new Agency();
+			agency.setCode("UYUVUYG");
+			Address address = new Address("2", "Washington street", "46544", "New York");
+			agency.setAddress(address);
+
+			em.persist(agency);
+
+			SimpleAccount simpleAccount = new SimpleAccount(5000, 0);
+			em.persist(simpleAccount);
+			ChargedAccount chargedAccount = new ChargedAccount(500, 2);
+			em.persist(chargedAccount);
+			agency.addAccount(simpleAccount);
+
+			Transaction transaction = new Transaction(new TransactionId(1L, 1L), Type.IPM, 200, 100);
+			transaction.setAccount(em.find(Account.class, 1L));
+			transaction.setAgency(em.find(Agency.class, 1L));
+			transaction.setAmount(500);
+			em.persist(transaction);
+
+//			Query query = em.createNamedQuery("select agency from Agency agency where agency.id=3");
+//			Agency test = (Agency) query.getSingleResult();
+//			System.out.println(test);
+
+			em.getTransaction().commit();
+
+//			em.getTransaction().begin();
+//			Agency a = em.find(Agency.class, 2L);
+//			a.setCode("ZZZZZ");
+//			em.merge(a);
+//			em.getTransaction().commit();
+//
+//			em.getTransaction().begin();
+//			Agency a2 = em.find(Agency.class, 1L);
+//			em.remove(a2);
+//			em.getTransaction().commit();
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+
+			if (emf != null) {
+				emf.close();
+			}
+		}
+
 	}
 }
--- src/main/java/otg/ss/bank/jpa/domain/Address.java
+++ src/main/java/otg/ss/bank/jpa/domain/Address.java
@@ -2,61 +2,71 @@ package otg.ss.bank.jpa.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Address implements Serializable {
+
+	@Column(name = "N")
 	private String number;
+	@Column(name = "street")
 	private String street;
+	@Column(name = "ZIP")
 	private String zipCode;
+	@Column(name = "City")
 	private String city;
-	
-	public Address() {}
-	
-	public Address( String number, String street, String zipCode, String city ) {
+
+	public Address() {
+	}
+
+	public Address(String number, String street, String zipCode, String city) {
 		this.number = number;
 		this.street = street;
 		this.zipCode = zipCode;
 		this.city = city;
 	}
-	
+
 	public String getNumber() {
 		return number;
 	}
-	
-	public void setNumber( String number ) {
+
+	public void setNumber(String number) {
 		this.number = number;
 	}
-	
+
 	public String getStreet() {
 		return street;
 	}
-	
-	public void setStreet( String street ) {
+
+	public void setStreet(String street) {
 		this.street = street;
 	}
-	
+
 	public String getZipCode() {
 		return zipCode;
 	}
-	
-	public void setZipCode( String zipCode ) {
+
+	public void setZipCode(String zipCode) {
 		this.zipCode = zipCode;
 	}
-	
+
 	public String getCity() {
 		return city;
 	}
-	
-	public void setCity( String city ) {
+
+	public void setCity(String city) {
 		this.city = city;
 	}
-	
+
 	@Override
 	public String toString() {
-		final StringBuffer sb = new StringBuffer( "Address{" );
-		sb.append( "number='" ).append( number ).append( '\'' );
-		sb.append( ", street='" ).append( street ).append( '\'' );
-		sb.append( ", zipCode='" ).append( zipCode ).append( '\'' );
-		sb.append( ", city='" ).append( city ).append( '\'' );
-		sb.append( '}' );
+		final StringBuffer sb = new StringBuffer("Address{");
+		sb.append("number='").append(number).append('\'');
+		sb.append(", street='").append(street).append('\'');
+		sb.append(", zipCode='").append(zipCode).append('\'');
+		sb.append(", city='").append(city).append('\'');
+		sb.append('}');
 		return sb.toString();
 	}
 }
